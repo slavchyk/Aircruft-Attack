8b8e2a52b46ca64549e63cd18bb55d546eaa74ac
kyrkaloRoman <kyrkalo.roman@gmail.com>
2015-04-02 01:35:05 +0300

    Add Scene, Control, Menu, Button

--- src/app/ACA/ACA.UniversalApp/ACA.UniversalApp.Shared/js/UI.js
+++ src/app/ACA/ACA.UniversalApp/ACA.UniversalApp.Shared/js/UI.js
@@ -4,4 +4,25 @@
     }
     );
     WinJS.Namespace.define("UI", { UI: UI });
+
+    var Scene = WinJS.Class.define(function (name) {
+        this.name = name;
+    });
+    WinJS.Namespace.define("UI.Scene", { Scene: Scene });
+
+    var Control = WinJS.Class.define(function (name) {
+        this.name = name;
+    });
+    WinJS.Namespace.define("UI.Control", { Control: Control });
+
+    var Menu = WinJS.Class.define(function (name) {
+        this.name = name;
+    });
+    WinJS.Namespace.define("UI.Control", { Menu: Menu });
+
+    var Button = WinJS.Class.define(function (name) {
+        this.name = name;
+    })
+    WinJS.Namespace.define("UI.Control", { Button: Button });
+
 })();
