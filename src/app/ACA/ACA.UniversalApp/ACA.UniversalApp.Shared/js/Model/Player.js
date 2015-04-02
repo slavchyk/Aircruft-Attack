4aafae1d5112c2ca39d8d38289af014587551ff1
kyrkaloRoman <kyrkalo.roman@gmail.com>
2015-04-03 00:43:46 +0300

    Add player class

---
+++ src/app/ACA/ACA.UniversalApp/ACA.UniversalApp.Shared/js/Model/Player.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var Player = WinJS.Class.define(function (name) {
+        this.name = name;
+    },
+    { Position: "" },
+    { Score: 0 },
+    {
+        Shoot: function () {
+
+        }
+    }
+    )
+})();
